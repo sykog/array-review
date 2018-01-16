4b9aa5bbb9b64ff84841b9cea800f40b926577cf
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-16 15:27:55 -0500

    intial commit

---
+++ array-review.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+     * Antonio Suarez
+     * 1/16/18
+     * http://asuarez.greenriverdev.com/328/PHPReview/array-review/array-review.php
+     */
+    // error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>php Review Assignment</title>
+    </head>
+    <body>
+    </body>
+</html>
